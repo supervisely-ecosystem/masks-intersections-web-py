334f4a3b5af4bc5368c7b4bffdb0da3d6ba27e9b
Nikolai Peukhov <petukhov.niko@gmail.com>
2024-12-19 00:00:32 -0300

    register button click handler for rendering

--- src/gui.py
+++ src/gui.py
@@ -3,3 +3,8 @@ from supervisely.app.widgets import Container, Button
 
 button = globals().get("button", Button("Click me!", widget_id="widget_3"))
 layout = globals().get("layout", Container(widgets=[button], widget_id="widget_4"))
+
+
+@button.click
+def button_click():
+    return
