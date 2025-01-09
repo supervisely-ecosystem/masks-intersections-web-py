8678d6b801464d87174ef2c308c695e1476eac62
Nikolai <petukhov.niko@gmail.com>
2025-01-09 02:37:11 +0000

    performance improvements

--- sly_sdk/webpy.py
+++ sly_sdk/webpy.py
@@ -194,23 +194,19 @@ class FigureObj:
 
 
 def get_figure_data(js_figure):
-    # img_cvs = js_figure._geometry._main.bitmap
-    # img_ctx = img_cvs.getContext("2d")
-    # t = time.perf_counter()
-    # img_data = img_ctx.getImageData(0, 0, img_cvs.width, img_cvs.height).data
-    # logger.info(f"getImageData time: {(time.perf_counter() - t) * 1000:.4f} ms")
-
     t = time.perf_counter()
     img_data_obj = js_figure._geometry._main.getPixels()
-    logger.info(f"getPixels time: {(time.perf_counter() - t) * 1000:.4f} ms")
+    logger.debug(f"getPixels time: {(time.perf_counter() - t) * 1000:.4f} ms")
 
     t = time.perf_counter()
     py_arr_data = img_data_obj.data.to_py()
-    logger.info(f"to_py time: {(time.perf_counter() - t) * 1000:.4f} ms")
+    logger.debug(f"to_py time: {(time.perf_counter() - t) * 1000:.4f} ms")
 
     t = time.perf_counter()
-    img_data = np.array(py_arr_data, dtype=np.uint8).reshape(img_data_obj.height, img_data_obj.width, 4)
-    logger.info(f"np.array time: {(time.perf_counter() - t) * 1000:.4f} ms")
+    img_data = np.array(py_arr_data, dtype=np.uint8).reshape(
+        img_data_obj.height, img_data_obj.width, 4
+    )
+    logger.debug(f"np.array time: {(time.perf_counter() - t) * 1000:.4f} ms")
 
     rgb = img_data[:, :, :3]
     alpha = img_data[:, :, 3]
@@ -230,9 +226,10 @@ def put_img_to_figure(js_figure, img_data: np.ndarray):
     try:
         js_figure._geometry._main.setImageData(new_img_data)
     except Exception as e:
+        raise e
+    finally:
         pixels_proxy.destroy()
         pixels_buf.release()
-        raise e
 
 
 def py_to_js(obj):
@@ -308,6 +305,7 @@ class WebPyApplication(metaclass=Singleton):
     def __init__(self, layout=None):
         if layout is None:
             from sly_sdk.app.widgets import Text
+
             layout = Text("", widget_id="__epmty_layout__")
         self.layout = layout
         self._run_f = None
@@ -350,7 +348,8 @@ class WebPyApplication(metaclass=Singleton):
         img_cvs = img_src.imageData
         img_ctx = img_cvs.getContext("2d")
         img_data = img_ctx.getImageData(0, 0, img_cvs.width, img_cvs.height).data
-        img_arr = np.array(img_data, dtype=np.uint8).reshape((img_cvs.height, img_cvs.width, 4))
+        py_arr_data = img_data.to_py()
+        img_arr = np.array(py_arr_data, dtype=np.uint8).reshape((img_cvs.height, img_cvs.width, 4))
         return img_arr
 
     def get_current_image_id(self):
@@ -414,7 +413,7 @@ class WebPyApplication(metaclass=Singleton):
         img = np.stack([geometry] * 4, axis=-1)
         t = time.perf_counter()
         put_img_to_figure(figure._js_obj, img)
-        logger.info(f"put_img_to_figure time: {(time.perf_counter() - t) * 1000:.4f} ms")
+        logger.debug(f"put_img_to_figure time: {(time.perf_counter() - t) * 1000:.4f} ms")
         new_version = figure._js_obj.geometry._main.version + 1
         self._store.dispatch(
             "figures/updateGeometryInFigure",
